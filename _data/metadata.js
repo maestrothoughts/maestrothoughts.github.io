edc7dfae7dab75bdb50e4b9c9545c1c14c0c505e
alvinchenn <alvinc101@gmail.com>
2025-03-02 23:49:41 -0800

    eleventy wip

---
+++ _data/metadata.js
@@ -0,0 +1,10 @@
+export default {
+  title: "Maestro Thoughts",
+  url: "https://alvinchenn.github.io/",
+  language: "en",
+  description: "This is a blog of whatever I feel like writing about.",
+  author: {
+    name: "Alvin Chen",
+    email: "chenalvin511@gmail.com",
+  },
+};
---
+++ content/about.md
@@ -0,0 +1,10 @@
+---js
+const eleventyNavigation = {
+	key: "About",
+	order: 3
+};
+---
+
+# About
+
+Hello! I'm Alvin, and this is my blog where I dump anything I think is worth sharing.
