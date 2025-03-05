a182590b8855dfcc96f6e8dfc78cbbbcf9fd886e
alvinchenn <alvinc101@gmail.com>
2025-03-04 21:07:11 -0800

    changed favicon

--- eleventy.config.js
+++ eleventy.config.js
@@ -27,7 +27,7 @@ export default async function (eleventyConfig) {
     })
     .addPassthroughCopy("./content/feed/pretty-atom-feed.xsl");
 
-  eleventyConfig.addPassthroughCopy({ "./public/img/favicon": "/" });
+  eleventyConfig.addPassthroughCopy({ "./favicon": "/" });
   // Run Eleventy when these files change:
   // https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
 
